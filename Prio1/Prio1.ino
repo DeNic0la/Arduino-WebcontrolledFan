30ad94ef7bc40111d0c2c9b553173edb5bec8fc6
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2020-03-05 13:45:46 +0100

    Update Prio1.ino

--- Prio1/Prio1.ino
+++ Prio1/Prio1.ino
@@ -1,28 +1,27 @@
-const int MosfetPin = 5;
-const int rotationSensor = 39;
-
+const int MosfetPin = 3;
+const int rotationSensor = 5;
+/*
 const int freq = 5000;
 const int Channel = 0;
 const int resolution = 16;
-
+*/
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(rotationSensor, INPUT);
   pinMode(MosfetPin, OUTPUT);
-
+/*
   ledcSetup(Channel, freq, resolution);
 
   ledcAttachPin(MosfetPin, Channel);
-  
+  */
 }
 
 void loop() {
   int rotVal = analogRead(rotationSensor);
   Serial.println(rotVal);
-  
   rotVal = rotVal/10;
-  int mappedRotVal = map(rotVal,0, 409,0,255);
-  ledcWrite(Channel, mappedRotVal);
-  delay(30);
+  int mappedRotVal = map(rotVal,0, 94,0,255);
+  analogWrite(MosfetPin, mappedRotVal);
+  delay(10);
 
 }
